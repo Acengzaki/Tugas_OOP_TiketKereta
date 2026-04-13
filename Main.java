7db2fd65ba42db98421f7c242b97f937c5be19af
Acengzaki <zakirohman955@gmaill.com>
2026-04-13 14:35:13 +0700

    tugas pertemuan 4

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 import java.util.InputMismatchException;
-import java.util.Scanner; // Import tambahan
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Inisialisasi
         Scanner scanner = new Scanner(System.in);
         DaftarKereta repoKereta = new DaftarKereta();
 
+        // 2. Tampilkan daftar kereta
         repoKereta.tampilkanDaftar();
 
         System.out.print("Pilih nomor kereta (1-3): ");
-        
+
         try {
             int pilihan = scanner.nextInt();
             
-            // Logika pemilihan
             Tiket tix = repoKereta.pilihKereta(pilihan);
 
             if (tix != null) {
@@ -26,8 +27,7 @@ public class Main {
                 System.out.println("\n[!] Error: Pilihan nomor " + pilihan + " tidak tersedia.");
             }
         } catch (InputMismatchException e) {
-            // Menangani jika user menginput selain angka
-            System.out.println("\n[!] Error: Input tidak valid. Mohon masukkan angka.");
+            System.out.println("\n[!] Error: Input tidak valid. Harap masukkan nomor kereta berupa angka.");
         } finally {
             scanner.close();
         }
